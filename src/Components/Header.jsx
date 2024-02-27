90b1491d4f0aa8a7e6eb2e117fc1268d0a7c9182
yuvarajj-dev <yuvarajj.developer@gmail.com>
2024-02-27 12:55:12 +0530

    First Commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,50 +1,39 @@
-import React, { useEffect } from 'react';
+import { useEffect } from "react";
 import "./Header.css";
-import { gsap } from 'gsap';
 
 export default function Header() {
-    useEffect(()=>{
-        const open = document.querySelector(".container");
-        const close   = document.querySelector(".close");
-        var tl = gsap.timeline({defaults:{duration:.8,ease:"expo.inOut"}});
-        
-        open.addEventListener("click",()=>{
-          if(tl.reversed()){
-            tl.play();
-          }else{
-            tl.to('nav',{right:0})
-              .to('nav',{height:'100vh'},'-=.1')
-              .to('nav ul li a',{opacity:1,pointerEvents:'all',stagger:0.2 },'-=.8')
-              .to('.close',{opacity:1,pointerEvents:'all'},'-=.8')
-              .to('nav h2',{opacity:1},'-=1');
-          }
-        });
 
-        close.addEventListener('click',()=>{
-          tl.reverse();
-        })
-        
 
-    },[]);
-   
+  useEffect(() => {
+    const nav_bar = document.querySelector(".nav_bar");
+    const handleclick = (e) => {
+      if (e.target.id === 'close') {
+        nav_bar.classList.remove('active');
+      } else {
+        nav_bar.classList.add('active');
+      }
+    }
+
+    nav_bar.addEventListener("click", handleclick);
+
+  }, [])
+
 
   return (
-    <div className='body'>
-      <div className="container ">
-          <div className="bars"></div>
-      </div>
-      <nav>
-          <h2>Yuvaraj J</h2>
-          <div className="close">
-            <div></div>
-          </div>
+    <>
+      <div className="main">
+        <div className="nav_bar">
+          <button id="close">&times;</button>
+          <h3 className="logo">Yuvaraj J</h3>
           <ul>
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
+            <li><a href="#">Portfolio</a></li>
             <li><a href="#">Projects</a></li>
             <li><a href="#">Contact</a></li>
           </ul>
-      </nav>
-    </div>
+        </div>
+      </div>
+    </>
   )
 }
