1b1c7c53c515011ba3cf8aa5fe14e3022599de81
yuvarajj-dev <yuvarajj.developer@gmail.com>
2024-03-02 12:37:39 +0530

    nav 2

--- src/Header/Navbar.jsx
+++ src/Header/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     <>
       <header>
         <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200 shadow-black dark:border-gray-600">
-          <ul className="navigation max-w-[90vw] flex flex-wrap justify-between items-center relative mx-auto py-8">
+          <ul className="navigation container  flex flex-wrap justify-between items-center relative mx-auto px-8 py-8">
             <a className="logo" href="#">
               <div><Anime/></div>
             </a>
