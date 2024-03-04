dde6d0e8a298047ba99f02642533c9cd2c1d8496
yuvarajj-dev <yuvarajj.developer@gmail.com>
2024-03-04 13:19:38 +0530

    04-03-2024 hero section

--- src/Home_file/Home.jsx
+++ src/Home_file/Home.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import "./Home.css";
 
 export default function () {
   return (
     <>
-      <div className='container'>
-        <div className="row">
-          <div>
-            <h1>Front-End React Developer</h1>
-            <p>Hi, I'm Stefan Topalovic. A passionate Front-end React Developer based in Belgrade, Serbia. </p>
+      <div className="container px-5 mx-auto lg:px-48 lg:flex md:flex lg:mt-24 self-center">
+          <div className='lg:w-1/2 mx-auto lg:py-9 md:py-9 '>
+            <div className="hero-text-section ">
+                  <h1>Front-End React Developer </h1>
+                  <img src="./Assest/icons8-waving-hand-light-skin-tone-96.png" alt="hello-icon" className='hello-icon' />
+                  <p>Hi, I'm Yuvaraj J. A passionate Front-end React Developer based in Tamilnadu, India.</p>
+                 
+            </div>
           </div>
-          <div>
-            <h1>Front-End React Developer</h1>
-            <p>Hi, I'm Stefan Topalovic. A passionate Front-end React Developer based in Belgrade, Serbia. </p>
+          <div className=' mx-auto'>
+            <div className="hero-img-section">
+                  <img src="./Assest/profile photo/stefan.webp" alt="hero-img" className='hero-img' />
+            </div>
           </div>
-        </div>
-
       </div>
     </>
   )
