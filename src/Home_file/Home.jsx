70a4ff6aa7a0f6ce4cc25b6ff4590f1a934e36b8
yuvarajj-dev <yuvarajj.developer@gmail.com>
2024-03-06 10:42:42 +0530

    06-05-2024 Home page and blob effect

--- src/Home_file/Home.jsx
+++ src/Home_file/Home.jsx
@@ -1,22 +1,28 @@
 import "./Home.css";
+import Blob from "./Profile Blob effect/Blob";
+
+
 export default function () {
 
   return (
     <>
       <div className="bg">
-        <div className="container mx-auto lg:px-36 flex justify-between py-10 ">
-          <div className="hero-text-div w-1/2">
+        <div className="container mx-auto lg:px-36 xl:flex lg:flex justify-between py-16  ">
+        <div className="lg:w-1/2 xl:w-1/2 lg:hidden xl:hidden mx-auto">
+              <Blob />
+          </div>
+          <div className="hero-text-div lg:w-1/2 xl:w-1/2 md:text-center ">
             <div className="">
-              <p className="text-5xl font-bold py-2 relative ">Front-End React Developer<img className="hand-img" src=".\Assest\icons8-waving-hand-light-skin-tone-96.png" alt="wave Icon" width="40" height="50"  /></p>
+              <p className="text-5xl font-bold py-2 relative md:px-16 ">Front-End React Developer<img className="hand-img" src=".\Assest\icons8-waving-hand-light-skin-tone-96.png" alt="wave Icon" width="45" height="55"  /></p>
             </div>
-            <p className="py-2">Hi, I'm Stefan Topalovic. A passionate Front-end React Developer based in Belgrade, Serbia. 📍</p>
-            <div className="flex gap-5 py-2">
+            <p className="py-2 md:px-28">Hi, I'm Stefan Topalovic. A passionate Front-end React Developer based in Tamilnadu, India. 📍</p>
+            <div className="flex gap-5 py-2 md:px-48">
               <img src="./Assest/gif/icons8-linkedin.gif" alt="LinkedIn Icon" width="40" height="50" />
               <img src="./Assest/gif/icons8-github.gif" alt="git Icon" width="40" height="50" />
             </div>
           </div>
-          <div>
-            <h1>hello</h1>
+          <div className="w-1/2 sm:hidden">
+              <Blob />
           </div>
         </div>
       </div>
---
+++ src/Home_file/Profile Blob effect/Blob.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Blob.css"
+export default function Blob() {
+    return (
+        <>
+            <div className="circle mx-auto">
+                <img src=".\Assest\profile photo\stefan.webp" alt="profile-pic" />
+            </div>
+        </>
+    )
+}
