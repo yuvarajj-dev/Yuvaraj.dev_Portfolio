5c4683e3882e8fff5f78f6a0e267b236d7f50a69
yuvarajj-dev <yuvarajj.developer@gmail.com>
2024-03-08 23:35:23 +0530

    home page completed 08-03-2024

--- src/Home_file/Home.jsx
+++ src/Home_file/Home.jsx
@@ -10,39 +10,40 @@ export default function () {
         <div className="container xl:mx-auto xl:px-52 xl:py-16 xl:flex
                                   lg:mx-auto lg:px-32 lg:py-16 lg:flex
                                   md:py-16 md:mx-auto
+                                  py-10  mx-auto
         ">
           {/* blog div for sm-md */}
           <div className="xl:hidden
                           lg:hidden  
                           md:block md:pb-10   
-                          sm:block
-                                        
+                          sm:block  
+                          mx-auto        
           ">
             <Blob />
           </div>
 
           {/* text-div for all */}
-          <div className="hero-text-div w-1/2 self-center
+          <div className="hero-text-div w-full self-center
                                         md:w-full        
           ">
-            <div className="relative flex items-center 
-
-            ">
-              <p className="xl:text-5xl xl:font-bold xl:py-5 
+            <div className="relative flex justify-center py-5 w-2/2 ">
+              <p className="text-3xl font-bold text-center 
+                            md:text-4xl md:font-bold md:py-2 md:text-center md:w-1/2 md:mx-auto
                             lg:text-5xl lg:font-bold lg:py-5 lg:text-start lg:w-full 
-                            md:text-4xl md:font-bold md:py-2 md:text-center md:w-1/2 md:mx-auto">
+                            xl:text-5xl xl:font-bold xl:py-5 
+                            ">
                 Front-End React Developer
               </p>
-
-              <img src=".\Assest\icons8-waving-hand-light-skin-tone-96.png" alt="waving-icon" className="hand-img" />
             </div>
             <p className="xl:text-xl xl:pb-3 
                           lg:text-xl lg:pb-3 lg:w-full lg:text-start
                           md:text-lg md:text-center md:w-1/1 md:mx-auto
+                          text-center 
             ">Hi, I'm Stefan Topalovic. A passionate Front-end React Developer based in Tamilnadu, India. 📍</p>
             <div className="xl:flex xl:gap-6 xl:items-center
                             lg:flex lg:gap-6 lg:items-center lg:justify-start
                             md:flex md:gap-6 md:items-center md:justify-center md:py-2
+                            flex gap-3 justify-center py-2
             ">
               <img src="./Assest/gif/icons8-linkedin.gif" alt="LinkedIn Icon" width="40" height="50" />
               <img src="./Assest/gif/icons8-github.gif" alt="git Icon" width="40" height="50" />
---
+++ src/Home_file/Texteffect/Texteff.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Texteff.css"
+
+export default function Texteff() {
+    return (
+        <>
+            <div className="txt" >
+                <b>F</b><b>r</b><b>o</b><b>n</b><b>t</b><b>-</b><b>E</b><b>n</b><b>d</b>
+                <b>R</b><b>e</b><b>a</b><b>c</b><b>t</b>
+                <b>D</b><b>e</b><b>v</b><b>e</b><b>l</b><b>o</b><b>p</b><b>e</b><b>r</b>
+            </div>
+        </>
+    )
+}
