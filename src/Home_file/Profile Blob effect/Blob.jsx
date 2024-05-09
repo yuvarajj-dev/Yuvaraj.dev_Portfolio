4312c0bdb00537db89ea08bef9f12b184532b598
yuvarajj-dev <yuvarajj.developer@gmail.com>
2024-05-09 13:01:57 +0530

    update

--- src/Home_file/Profile Blob effect/Blob.jsx
+++ src/Home_file/Profile Blob effect/Blob.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-// import "./Blob.css"
+import "./Blob.css"
 export default function Blob() {
     return (
         <>
-            {/* <div class="box mx-auto">
+            <div class="box mx-auto">
                     <img src=".\Assest\profile photo\Yuvaraj.jpg" alt="profile-pic" className='rainbow-img' />
-            </div> */}
+            </div>
         </>
     )
 }
